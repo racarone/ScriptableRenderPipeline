ffe3742fffe68a8e1c66f033c73cb0f8db86a94f
Robert Carone <racarone@gmail.com>
2020-03-04 16:51:50 +0000

    [SU] Disable pre-exposure, enable precomputed atmospheric attenuation, disable area lights

--- com.unity.render-pipelines.high-definition-config/Runtime/ShaderConfig.cs
+++ com.unity.render-pipelines.high-definition-config/Runtime/ShaderConfig.cs
@@ -16,8 +16,8 @@ namespace UnityEngine.Rendering.HighDefinition
     public enum ShaderOptions
     {
         CameraRelativeRendering = 1, // Rendering sets the origin of the world to the position of the primary (scene view) camera
-        PreExposition = 1,
-        PrecomputedAtmosphericAttenuation = 0, // Precomputes atmospheric attenuation for the directional light on the CPU, which makes it independent from the fragment's position, which is faster but wrong
+        PreExposition = 0,
+        PrecomputedAtmosphericAttenuation = 1, // Precomputes atmospheric attenuation for the directional light on the CPU, which makes it independent from the fragment's position, which is faster but wrong
 #if ENABLE_RAYTRACING
         Raytracing = 1,
 #else
@@ -28,7 +28,7 @@ namespace UnityEngine.Rendering.HighDefinition
 #else
         XrMaxViews = 1,
 #endif
-        AreaLights = 1,
+        AreaLights = 0,
 
         DeferredShadowFiltering = HDShadowFilteringQuality.Medium,
         BarnDoor = 0
